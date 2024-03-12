aa7283c162cb05a89a86f679448becaf2f0044b9
s26776 <s26776@pjwstk.edu.pl>
2024-03-12 13:45:46 -0400

    change name of loop variable from i to j

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ class Hello
     static double getAverage(int[] arr)
     {
         double sum = 0;
-        for (int i = 0; i < arr.Length; i++) {
-            sum += arr[i];
+        for (int j = 0; j < arr.Length; j++) {
+            sum += arr[j];
         }
         return sum/arr.Length;
     }
