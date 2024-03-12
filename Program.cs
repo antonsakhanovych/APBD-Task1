cfda11a54c74c00777a1bc8481a0abce5486b738
s26776 <s26776@pjwstk.edu.pl>
2024-03-12 13:35:27 -0400

    implement average function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 class Hello
 {
+    static double getAverage(int[] arr)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.Length; i++) {
+            sum += i;
+        }
+        return sum/arr.Length;
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
@@ -9,5 +18,6 @@ class Hello
         for (int i = 0; i < 3; i++) {
             Console.WriteLine(sNumber + " loves APBD! and " + sNumber + " is " + age + " years old." );
         }
+
     }
 }
